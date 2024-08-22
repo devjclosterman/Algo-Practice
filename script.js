1283bf73186e33da9028621d9d92c28ef8f980ce
JSON <devjclosterman@gmail.com>
2024-08-21 19:33:42 -0700

    algos part 2

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const sevenBoom = arr => /7/.test(arr) ? 'Boom!' : 'There is no 7 in the array';
 
 console.log(sevenBoom([1, 2, 3, 4, 5, 6, 6])); //Boom 
 
+const getLength = arr => arr.flat(Infinity).length;
+
+console.log(getLength([1, [2, [4, [5, 6]]]])) // 6
